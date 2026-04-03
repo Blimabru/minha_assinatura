659a63c6e5220743b4c0874abeba171fc73aa7f6
Bruno Moreira Lima <bru.no@outlook.com.br>
2026-04-03 12:25:08 -0300

    chore: habilitar decorators para WatermelonDB

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,7 +1,14 @@
 module.exports = function (api) {
+  // Mantém cache da configuração para melhorar performance do Metro/Babel.
   api.cache(true);
+
   return {
+    // Preset padrão do Expo para transpilar React Native.
     presets: ['babel-preset-expo'],
-    plugins: [["@babel/plugin-proposal-decorators", { "legacy": true }]]
+    plugins: [
+      // Necessário para os decorators legacy usados pelo WatermelonDB
+      // (@text, @field, @relation, etc).
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ],
   };
 };
